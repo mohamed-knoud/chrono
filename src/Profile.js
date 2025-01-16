a19a017a61158c9a3ff608c6ce796e5c2d0d6b98
mohamed-knoud <117742106+mohamed-knoud@users.noreply.github.com>
2025-01-16 10:26:50 +0100

    Update Profile.js

--- src/Profile.js
+++ src/Profile.js
@@ -1592,10 +1592,10 @@ upsd.current.style.display='none'
 
                 setLoader2(false);  // Correctly update loader state
               } else {
-                console.error('Response data is empty or not an array:', response.data);
+                console.error('Response data is empty or not an array:', newMessagess2.data);
               }
             } else {
-              console.error('API response did not contain expected data:', response);
+              console.error('API response did not contain expected data:', newMessagess2);
             }
           } catch (error) {
             console.error('Error occurred during API call:', error);
