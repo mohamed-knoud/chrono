75a811446451315c8dc3841544c0fe7f136c39d4
mohamed-knoud <117742106+mohamed-knoud@users.noreply.github.com>
2025-01-16 19:15:10 +0100

    Update Header.js

--- src/Header.js
+++ src/Header.js
@@ -1742,7 +1742,7 @@ up.current.style.display='none'
     res.data.response.id === item.id_dest ? 
       <div key={index} id={item.id} style={{fontSize:'0.9em',alignSelf:'flex-end',width:'60%',color:'white',borderRadius:'5px',margin:'10px 10px',padding:'10px',backgroundColor:'#0c6dfd'}}>
         <span>{item.content}</span><br/>
-        <span>{item.moment}</span>
+        <span style={{fontSize:'0.8em'}}>{item.moment}</span>
       </div> :
       <div key={index} id={item.id} style={{fontSize:'0.9em',alignSelf:'flex-start',width:'60%',color:'black',borderRadius:'5px',margin:'10px 10px',padding:'10px',border:'1px solid rgba(0,0,0,0.5)',backgroundColor:'#FFF'}}>
         <span>{item.content}</span><br/>
