8b59eff3492ed17cf8794c4307a0ee8dfdf46c12
mohamed-knoud <117742106+mohamed-knoud@users.noreply.github.com>
2025-01-16 11:14:23 +0100

    Update Profile.js

--- src/Profile.js
+++ src/Profile.js
@@ -1638,67 +1638,67 @@ upsd.current.style.display='none'
       subtree: true,
       characterData: true
   };
-   useEffect(() => {
-      let data;
-      // console.log(11)
-      if (res && username2!=="") {
-        data = { id_exp: res.data.response.id, username: username2 };
-          console.log(data)
-        if (!loader2 && viss && msg.current) {
-          if(newMessagess && newMessagess.length!==0)
-            {
-              // console.log(111)
-              window.location.href = `#${newMessagess[newMessagess.length-1].id}`
-              setLoader2(false)
-            }
-        }
-        if (msg.current) {
-          observer.observe(msg.current, config)
-        }
-        const fetch = async () => {
-          try {
-            let newMessagess2 = await axios.post('https://soc-net.info/api/checkNewMessage3.php', data, {
-              headers: {
-                'Content-Type': 'application/json',
-              },
-            });
+   // useEffect(() => {
+   //    let data;
+   //    // console.log(11)
+   //    if (res && username2!=="") {
+   //      data = { id_exp: res.data.response.id, username: username2 };
+   //        console.log(data)
+   //      if (!loader2 && viss && msg.current) {
+   //        if(newMessagess && newMessagess.length!==0)
+   //          {
+   //            // console.log(111)
+   //            window.location.href = `#${newMessagess[newMessagess.length-1].id}`
+   //            setLoader2(false)
+   //          }
+   //      }
+   //      if (msg.current) {
+   //        observer.observe(msg.current, config)
+   //      }
+   //      const fetch = async () => {
+   //        try {
+   //          let newMessagess2 = await axios.post('https://soc-net.info/api/checkNewMessage3.php', data, {
+   //            headers: {
+   //              'Content-Type': 'application/json',
+   //            },
+   //          });
   
-            // console.log('API Response:', response);
-            newMessagess = newMessagess2.data
-            if (newMessagess) {
-              // console.log('Data exists:', newMessagess);
-              // window.location.href = `#${response.data[response.data.length-1].id}`
+   //          // console.log('API Response:', response);
+   //          newMessagess = newMessagess2.data
+   //          if (newMessagess) {
+   //            // console.log('Data exists:', newMessagess);
+   //            // window.location.href = `#${response.data[response.data.length-1].id}`
+   //            console
+   //            //   window.location.href = `#${newMessagess[newMessagess.length-1].id}`
+  
+   //            if (Array.isArray(newMessagess) && newMessagess.length !== 0) {
+   //              // console.log('Before updating state:', newMessagess); // Log before state update
+   //              setNewMessagess(newMessagess); // Update state properly
               
-              //   window.location.href = `#${newMessagess[newMessagess.length-1].id}`
-  
-              if (Array.isArray(newMessagess) && newMessagess.length !== 0) {
-                // console.log('Before updating state:', newMessagess); // Log before state update
-                setNewMessagess(newMessagess); // Update state properly
-              
-                // console.log('State updated:', newMessagess); // Log after state update
-                // if(!isArrayEmpty(newMessagess))X
+   //              // console.log('State updated:', newMessagess); // Log after state update
+   //              // if(!isArrayEmpty(newMessagess))X
 
-                  // window.location.href = `#${newMessagess[newMessagess.length-1].id}`
+   //                // window.location.href = `#${newMessagess[newMessagess.length-1].id}`
 
-                setLoader2(false);  // Correctly update loader state
-              } else {
-                console.error('Response data is empty or not an array:', newMessagess2.data);
-              }
-            } else {
-              console.error('API response did not contain expected data:', newMessagess2);
-            }
-          } catch (error) {
-            console.error('Error occurred during API call:', error);
-          }
-        };
+   //              setLoader2(false);  // Correctly update loader state
+   //            } else {
+   //              console.error('Response data is empty or not an array:', newMessagess2.data);
+   //            }
+   //          } else {
+   //            console.error('API response did not contain expected data:', newMessagess2);
+   //          }
+   //        } catch (error) {
+   //          console.error('Error occurred during API call:', error);
+   //        }
+   //      };
   
-        // Set an interval to keep fetching new messages
-        checkNewMessages2 = setInterval(fetch, 1000);
-        return () => {
-          clearInterval(checkNewMessages2); // Cleanup the interval on component unmount or dependency change
-        };
-      }
-    }, [username, viss]);
+   //      // Set an interval to keep fetching new messages
+   //      checkNewMessages2 = setInterval(fetch, 1000);
+   //      return () => {
+   //        clearInterval(checkNewMessages2); // Cleanup the interval on component unmount or dependency change
+   //      };
+   //    }
+   //  }, [username, viss]);
     
     useEffect(() => {
       // if(username!=null){
