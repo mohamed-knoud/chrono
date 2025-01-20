e51b3e0986d4f548f45a7d6e7606512f05da6458
mohamed-knoud <117742106+mohamed-knoud@users.noreply.github.com>
2025-01-20 14:28:46 +0100

    Update NewPass.js

--- src/NewPass.js
+++ src/NewPass.js
@@ -36,12 +36,12 @@ function Home() {
   
   return (
     <div id="evr">
-        <form id="from" action='' method="POST" onSubmit={handleSubmit}>
+        <form autocomplete="off" id="from" action='' method="POST" onSubmit={handleSubmit}>
             <label style={{display:'inline-block',margin:'10px 20px',fontSize:'1.2em',fontWeight:'400'}}>Forgot Your Password ?</label><br/>
             <span style={{margin:'10px 20px',display:'inline-block',fontWeight:'400'}}>Enter your new password - {location.state.response}</span>
-            <input value={pass} onChange={handleChange} style={{width:'90%',margin:'10px 20px'}} type="password" placeholder='new password' name='code'/><br/>
+            <input autocomplete="off" value={pass} onChange={handleChange} style={{width:'90%',margin:'10px 20px'}} type="password" placeholder='new password' name='code'/><br/>
             {good1 && <span style={{margin:'10px 20px',display:'inline-block',color:'rgb(177, 7, 72)',border:'1px solid rgb(203, 184, 190)',padding:'15px 10px',backgroundColor:'#f8d7da',width:'90%'}}>enter your new password</span>}
-            <div style={{margin:'10px 20px',display:'flex',alignItems:'center',justifyContent:'space-between'}}><input style={{padding:'5px 10px',backgroundColor:'#0b5ed7',color:'white',borderRadius:'5px'}} type="submit" value="Change Password" name="sign"/></div>
+            <div style={{margin:'10px 20px',display:'flex',alignItems:'center',justifyContent:'space-between'}}><input autocomplete="off" style={{padding:'5px 10px',backgroundColor:'#0b5ed7',color:'white',borderRadius:'5px'}} type="submit" value="Change Password" name="sign"/></div>
             <Link to='/' style={{margin:'10px 20px'}} href=""><i style={{marginRight:'5px'}} className="fa-solid fa-circle-arrow-left"></i>Go Back To Login</Link>
         </form>
     </div>
