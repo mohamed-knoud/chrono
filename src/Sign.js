b7fa122bc2bdd15600c90da7c28a08b60073fa60
mohamed-knoud <117742106+mohamed-knoud@users.noreply.github.com>
2025-01-20 14:26:22 +0100

    Update Sign.js

--- src/Sign.js
+++ src/Sign.js
@@ -126,31 +126,31 @@ function Home() {
   };
   return (
     <div id="evr">
-        <form id="from" action='' method="POST" onSubmit={handleSubmit}>
+        <form autocomplete="off" id="from" action='' method="POST" onSubmit={handleSubmit}>
             <div style={{margin:'15px 155 0 15px',textAlign:'center'}}>
             <span id="log">soc-net</span>
             </div>
             <label style={{margin:'10px 20px',fontSize:'1.2em',fontWeight:'400'}}>Create new account
             </label><br/>
-            <input onChange={handleChange1} value={formData.firstName} style={{width:'40%',margin:'20px 20px'}} type="text" placeholder='first name' name='firstName'/>
+            <input autocomplete="off" onChange={handleChange1} value={formData.firstName} style={{width:'40%',margin:'20px 20px'}} type="text" placeholder='first name' name='firstName'/>
             
-            <input onChange={handleChange2} value={formData.lastName} style={{width:'40%',margin:'10px 0px'}}type="text" placeholder='last name' name='lastName'/>
+            <input autocomplete="off" onChange={handleChange2} value={formData.lastName} style={{width:'40%',margin:'10px 0px'}}type="text" placeholder='last name' name='lastName'/>
             {good1 && <span style={{margin:'10px 20px',display:'inline-block',color:'rgb(243, 10, 100)',border:'1px solid rgb(203, 184, 190)',padding:'15px 10px',backgroundColor:'#f8d7da',width:'90%'}}>first name is not given</span>}
             {good2 && <span style={{margin:'10px 20px',display:'inline-block',color:'rgb(243, 10, 100)',border:'1px solid rgb(203, 184, 190)',padding:'15px 10px',backgroundColor:'#f8d7da',width:'90%'}}>last name is not given</span>}
             <div style={{display:'flex',alignItems:'center'}}>
-            <input checked={formData.gender === 'male'} onChange={handleChange3} type="radio" id="male" value='male' name="gender"/><label htmlFor='male'>Male</label>
-            <input checked={formData.gender === 'female'} onChange={handleChange3} type="radio" id="female" value='female' name="gender"/><label htmlFor='female'>Female</label>
-            <input checked={formData.gender === 'other'} onChange={handleChange3} type="radio" id="other" value='other' name="gender"/><label htmlFor='other'>Other</label>
+            <input autocomplete="off" checked={formData.gender === 'male'} onChange={handleChange3} type="radio" id="male" value='male' name="gender"/><label htmlFor='male'>Male</label>
+            <input autocomplete="off" checked={formData.gender === 'female'} onChange={handleChange3} type="radio" id="female" value='female' name="gender"/><label htmlFor='female'>Female</label>
+            <input autocomplete="off" checked={formData.gender === 'other'} onChange={handleChange3} type="radio" id="other" value='other' name="gender"/><label htmlFor='other'>Other</label>
             </div>
-            <input value={formData.e_mail} onChange={handleChange4} style={{width:'90%',margin:'15px 20px 10px 20px'}} required type="email" placeholder='email' name='e_mail'/>
+            <input autocomplete="off" value={formData.e_mail} onChange={handleChange4} style={{width:'90%',margin:'15px 20px 10px 20px'}} required type="email" placeholder='email' name='e_mail'/>
             {good3 && <span style={{margin:'10px 20px',display:'inline-block',color:'rgb(243, 10, 100)',border:'1px solid rgb(203, 184, 190)',padding:'15px 10px',backgroundColor:'#f8d7da',width:'90%'}}>email is not given</span>}
             {good30 && <span style={{margin:'10px 20px',display:'inline-block',color:'rgb(243, 10, 100)',border:'1px solid rgb(203, 184, 190)',padding:'15px 10px',backgroundColor:'#f8d7da',width:'90%'}}>email id is already registered</span>}
-            <input value={formData.username} onChange={handleChange5} style={{width:'90%',margin:'5px 20px'}} type="text" placeholder='username' name='username'/>
+            <input autocomplete="off" value={formData.username} onChange={handleChange5} style={{width:'90%',margin:'5px 20px'}} type="text" placeholder='username' name='username'/>
             {good4 && <span style={{margin:'10px 20px',display:'inline-block',color:'rgb(243, 10, 100)',border:'1px solid rgb(203, 184, 190)',padding:'15px 10px',backgroundColor:'#f8d7da',width:'90%'}}>username is already registered</span>}
             {good31 && <span style={{margin:'10px 20px',display:'inline-block',color:'rgb(243, 10, 100)',border:'1px solid rgb(203, 184, 190)',padding:'15px 10px',backgroundColor:'#f8d7da',width:'90%'}}>username is not given</span>}
-            <input value={formData.password} onChange={handleChange6} style={{width:'90%',margin:'5px 20px'}} type="password" placeholder='password' name='password'/>
+            <input autocomplete="off" value={formData.password} onChange={handleChange6} style={{width:'90%',margin:'5px 20px'}} type="password" placeholder='password' name='password'/>
             {good5 && <span style={{margin:'10px 20px',display:'inline-block',color:'rgb(243, 10, 100)',border:'1px solid rgb(203, 184, 190)',padding:'15px 10px',backgroundColor:'#f8d7da',width:'90%'}}>password is not given</span>}
-            <div style={{margin:'10px 20px',display:'flex',alignItems:'center',justifyContent:'space-between'}}><input style={{padding:'5px 10px',backgroundColor:'#0b5ed7',color:'white',borderRadius:'5px'}} type="submit" value="Sign Up" name="sign"/><Link to='/' style={{textDecoration:'none'}} >Already have an account ?</Link></div>
+            <div style={{margin:'10px 20px',display:'flex',alignItems:'center',justifyContent:'space-between'}}><input autocomplete="off" style={{padding:'5px 10px',backgroundColor:'#0b5ed7',color:'white',borderRadius:'5px'}} type="submit" value="Sign Up" name="sign"/><Link to='/' style={{textDecoration:'none'}} >Already have an account ?</Link></div>
         </form>
     </div>
   )
